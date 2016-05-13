35175babc9c6e7f628c0069895d2479b53978893
Seth Frey <moctodliamg@gmail.com>
2016-05-13 19:18:04 +0000

    cusotmization for getting this running and saving data to repo.

--- examples/sniff.sh
+++ examples/sniff.sh
@@ -1,2 +1,2 @@
-sort -R ~/projecto/research_projects/mcscraper/in/master_ip_list.txt | head -n 100 > testin.txt
-python frey_bulk_info.py testin.txt log > testoutinfo.json
+tac ~/projecto/research_projects/mcscraper/in/master_ip_list.txt > in_servers.txt
+python frey_bulk_info.py in_servers.txt log > out_servers.json
