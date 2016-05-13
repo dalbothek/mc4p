a5fceb9a602fdc462c430656d18ed64b6affabd0
Seth Frey <moctodliamg@gmail.com>
2016-05-13 19:20:28 +0000

    small script change

--- examples/sniff.sh
+++ examples/sniff.sh
@@ -1,2 +1,2 @@
 tac ~/projecto/research_projects/mcscraper/in/master_ip_list.txt > in_servers.txt
-python frey_bulk_info.py in_servers.txt log > out_servers.json
+python frey_bulk_info.py in_servers.txt log >> out_servers.json
